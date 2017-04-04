0d84e725827e16d76be473704b9e54769008e261
ishmilan <isma.milan.marquez@gmail.com>
2017-04-05 00:01:35 +0200

    Adding welcome page(provisional)

---
+++ application/views/footer.php
@@ -0,0 +1,5 @@
+    <footer></footer>
+  </body>
+  <script src="/hinato/assets/js/jquery-3.2.0.min.js"></script>
+  <script src="/hinato/assets/js/bootstrap.min.js"></script>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="title" content="H·I·N·A·T·O! - KanBan WebApp">
+    <meta name="description" content="HINATO Is Not Another Task Organizer. A Web App with a license GPLv3">
+    <title>H·I·N·A·T·O!</title>
+    <link rel="stylesheet" href="/hinato/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="/hinato/assets/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="/hinato/assets/css/main.css">
+    <link rel="shortcut icon" href="/hinato/assets/img/favicon.png">
+  </head>
+  <body>
+    <header>
+      <a href="/hinato/">
+        <img src="/hinato/assets/img/logo.png" alt="" class="img-responsive">
+      </a>
+      <div class="dropdown" id="user-menu" style="float:right;">
+        <button class="dropdown-toggle glyphicon glyphicon-menu-hamburger header-menu" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true"></button>
+        <ul class="dropdown-menu user-dropdown" aria-labelledby="dropdownMenu1">
+          <li>option 1</li>
+          <li>option 2</li>
+          <li>option 3</li>
+        </ul>
+      </div>
+    </header>
+  </body>
