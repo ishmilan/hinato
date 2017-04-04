ae8f94df22dd04fd3575fca27bb34b13020014cd
ishmilan <isma.milan.marquez@gmail.com>
2017-04-05 00:15:18 +0200

    adding meta:viewport tag

--- application/views/header.php
+++ application/views/header.php
@@ -7,6 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
     <meta name="title" content="H·I·N·A·T·O! - KanBan WebApp">
     <meta name="description" content="HINATO Is Not Another Task Organizer. A Web App with a license GPLv3">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>H·I·N·A·T·O!</title>
     <link rel="stylesheet" href="/hinato/assets/css/bootstrap.css">
     <link rel="stylesheet" href="/hinato/assets/css/bootstrap-theme.css">
